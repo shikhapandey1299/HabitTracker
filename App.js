9742786ba0b73c00f2d2163366c449ae2b9b4247
shikhapandey1299 <52201894+shikhapandey1299@users.noreply.github.com>
2023-03-20 15:49:15 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import HabitContainer from "./components/habitContainer";
+
+function App() {
+  return (
+    <div>
+      <div className="App">
+			<HabitContainer />
+		</div>
+    </div>
+  );
+}
+
+export default App;
